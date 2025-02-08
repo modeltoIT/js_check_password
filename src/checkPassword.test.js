6b3c6a06e7631b5867e57736d27e92509da616e6
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-02-08 09:36:29 +0400

    add solution

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -8,12 +8,50 @@ describe(`Function 'checkPassword':`, () => {
   });
 
   it(`should return boolean`, () => {
-
+    expect(typeof checkPassword('Password1!')).toBe('boolean');
   });
 
   it(`should return 'true' for the valid password with 8 characters`, () => {
 
   });
 
-  // write more tests here
+  it(`should return true for valid password`, () => {
+    expect(checkPassword('P@ssword1')).toBeTruthy();
+  });
+
+  it(`should contain at least one number`, () => {
+    expect(checkPassword('P@ssword')).toBeFalsy();
+  });
+
+  it(`should accept\`Aa-Яя\` letters`, () => {
+    expect(checkPassword('ЯP@ssword1я')).toBeFalsy();
+  });
+
+  it(`should be at least 8 chars long`, () => {
+    expect(checkPassword('P@ss123')).toBeFalsy();
+  });
+
+  it(`should not be more than 16 chars long`, () => {
+    expect(checkPassword('P@ss1234567891011')).toBeFalsy();
+  });
+
+  it(`should accept 16 chars length`, () => {
+    expect(checkPassword('P@ss123456789101')).toBeTruthy();
+  });
+
+  it(`should not accept spaces`, () => {
+    expect(checkPassword('P@ss12ssd f')).toBeFalsy();
+  });
+
+  it(`should not contain at least 1 digit`, () => {
+    expect(checkPassword('P@ssssdf')).toBeFalsy();
+  });
+
+  it(`should not contain at least 1 special character`, () => {
+    expect(checkPassword('Pssssdf1')).toBeFalsy();
+  });
+
+  it(`should not contain at least 1 uppercase letter`, () => {
+    expect(checkPassword('p@ssssdf1')).toBeFalsy();
+  });
 });
